88c9d1e1b4ca3675361a1f1fa816b6e9bf20178c
Javier Mendonça <javier.jmc@gmail.com>
2018-06-13 11:05:02 +0200

    Fixed endpoint.

--- js/eos-alarm.js
+++ js/eos-alarm.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-let invalidPrototcolRegex = /^(%20|\s)*(javascript|data)/im;
-let ctrlCharactersRegex = /[^\x20-\x7E]/gmi;
-let urlSchemeRegex = /^([^:]+):/gm;
+let invalidPrototcolRegex = /^(%20|\s)*(javascript|data)/im
+let ctrlCharactersRegex = /[^\x20-\x7E]/gmi
+let urlSchemeRegex = /^([^:]+):/gm
 let relativeFirstCharacters = ['.', '/']
 
 var favoriteBlockProducerName = 'eosswedenorg'
@@ -12,82 +12,61 @@ function isRelativeUrl(url) {
 }
 
 function sanitizeUrl(url) {
-    let urlScheme, urlSchemeParseResults;
-    let sanitizedUrl = url.replace(ctrlCharactersRegex, '');
+    let urlScheme, urlSchemeParseResults
+    let sanitizedUrl = url.replace(ctrlCharactersRegex, '')
 
     if (isRelativeUrl(sanitizedUrl)) {
-        return sanitizedUrl;
+        return sanitizedUrl
     }
 
-    urlSchemeParseResults = sanitizedUrl.match(urlSchemeRegex);
+    urlSchemeParseResults = sanitizedUrl.match(urlSchemeRegex)
     if (!urlSchemeParseResults) {
-        return 'about:blank';
+        return 'about:blank'
     }
 
-    urlScheme = urlSchemeParseResults[0];
+    urlScheme = urlSchemeParseResults[0]
     if (invalidPrototcolRegex.test(urlScheme)) {
-        return 'about:blank';
+        return 'about:blank'
     }
 
-    return sanitizedUrl;
+    return sanitizedUrl
 }
 
 const networks = [
     {
         name: "Main Net",
-        host: "node2.liquideos.com",
-        port: 80,
-        chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-        secured: false
-    },
-    {
-        name: "Main Net SSL",
-        host: "node2.liquideos.com",
-        port: 443,
-        scatterPort: 80,
-        chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
-        secured: true
-    },
-    {
-        name: "Main Net SSL - for scatter over ssl",
-        host: "node2.liquideos.com",
+        host: "fn.eossweden.se",
         port: 443,
         chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906",
         secured: true
     },
-    {
-        name: "Jungle Testnet",
-        host: "dolphin.eosblocksmith.io",
-        chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca",
-        port: 8888
-    }
-];
+]
 
-var defaultIndex = 0;
+var defaultIndex = 0
 if (this.location.protocol === "https:") {
-    defaultIndex = 1;
+    defaultIndex = 1
 }
 
 function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
+    if (!url) url = window.location.href
+    name = name.replace(/[\[\]]/g, "\\$&")
     var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+        results = regex.exec(url)
+    if (!results) return null
+    if (!results[2]) return ''
+    return decodeURIComponent(results[2].replace(/\+/g, " "))
 }
 
-var networkParam = getParameterByName('network');
+var networkParam = getParameterByName('network')
 if (networkParam)
-    defaultIndex = networkParam;
-const network = networks[defaultIndex];
+    defaultIndex = networkParam
+const network = networks[defaultIndex]
 
 var eosAlarm = class {
 
     addTd(text) {
-        var td = document.createElement('td');
-        td.innerHTML = text;
+        var td = document.createElement('td')
+        td.innerHTML = text
         return td;
     }
 
@@ -122,9 +101,9 @@ var eosAlarm = class {
         };
 
         this.eosPublic = new Eos(config);
-        this.populateBlockProducers().then(function (result) {
-            console.log(JSON.stringify(result, null, "    "))
-            this.buildTable(result);
+        this.populateBlockProducers().then((result) => {
+            console.log(result)
+            this.buildTable(result)
         }, this.handleError)
     }
 
@@ -137,56 +116,55 @@ var eosAlarm = class {
         document.getElementById('error').innerText = error.message
     }
 
-
     buildTable(result) {
-        this.countTotalVotes(res);
-        let table = document.getElementsByTagName('tbody')[0];
-        let sorted = res.rows.sort((a, b) => Number(a.total_votes) > Number(b.total_votes) ? -1 : 1);
+        this.countTotalVotes(result)
+        let table = document.getElementsByTagName('tbody')[0]
+        let sorted = result.rows.sort((a, b) => Number(a.total_votes) > Number(b.total_votes) ? -1 : 1)
 
         for (let i = 0; i < sorted.length; i++) {
-            let row = sorted[i];
-            let rowSanitized = sanitizeUrl(row.url);
-            let tr = document.createElement('tr');
-            table.append(tr);
-            tr.append(this.addTd('<input name="bpVote" type="radio" value="' + row.owner + '" ' + (row.owner === favoriteBlockProducerName ? 'checked' : '') + ' >'));
-            tr.append(this.addTd(i + 1));
-            tr.append(this.addTd("<a href='" + rowSanitized + "'>" + row.owner + "</a>"));
-            tr.append(this.addTd(this.cleanNumber(row.total_votes)));
-            tr.append(this.addTd(this.createProgressBar(this.cleanPercent(this.voteNumber(row.total_votes) / this.votes))));
+            let row = sorted[i]
+            let rowSanitized = sanitizeUrl(row.url)
+            let tr = document.createElement('tr')
+            table.append(tr)
+            tr.append(this.addTd('<input name="bpVote" type="radio" value="' + row.owner + '" ' + (row.owner === favoriteBlockProducerName ? 'checked' : '') + ' >'))
+            tr.append(this.addTd(i + 1))
+            tr.append(this.addTd("<a href='" + rowSanitized + "'>" + row.owner + "</a>"))
+            tr.append(this.addTd(this.cleanNumber(row.total_votes)))
+            tr.append(this.addTd(this.createProgressBar(this.cleanPercent(this.voteNumber(row.total_votes) / this.votes))))
 
             if (row.owner === favoriteBlockProducerName) checkRanking(i + 1)
         }
 
         document.getElementsByName("bpFollow").forEach(e => {
-            e.onclick = this.selectBlockProducer;
-        });
+            e.onclick = this.selectBlockProducer
+        })
 
         this.selectBlockProducer()
         return table;
     }
 
-    countTotalVotes(res) {
+    countTotalVotes(result) {
         this.votes = 0;
-        for (let i = res.rows.length - 1; i >= 0; i--) {
-            this.votes += this.voteNumber(res.rows[i].total_votes);
+        for (let i = result.rows.length - 1; i >= 0; i--) {
+            this.votes += this.voteNumber(result.rows[i].total_votes)
         }
     }
 
     search() {
-        let input, filter, table, tr, td, i;
-        input = document.getElementById("search");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("bps");
-        tr = table.getElementsByTagName("tr");
+        let input, filter, table, tr, td, i
+        input = document.getElementById("search")
+        filter = input.value.toUpperCase()
+        table = document.getElementById("bps")
+        tr = table.getElementsByTagName("tr")
 
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 1; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
+            td = tr[i].getElementsByTagName("td")[1]
             if (td) {
                 if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
+                    tr[i].style.display = ""
                 } else {
-                    tr[i].style.display = "none";
+                    tr[i].style.display = "none"
                 }
             }
         }
@@ -207,16 +185,16 @@ var eosAlarm = class {
     }
 
     cleanPercent(num) {
-        return Math.round(num * 10000) / 100 + "%";
+        return Math.round(num * 10000) / 100 + "%"
     }
 }
 
 function refresh() {
-    eosAlarm.refreshBlockProducers();
+    eosAlarm.refreshBlockProducers()
 }
 
 var eosAlarm = new eosAlarm();
-eosAlarm.refreshBlockProducers();
+eosAlarm.refreshBlockProducers()
 
 function checkRanking(rank) {
     var audio
